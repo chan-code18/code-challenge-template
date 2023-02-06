00ed92dd007e46a2c3f11a2d3dc93a39e3d149ed
chan-code18 <124629887+chan-code18@users.noreply.github.com>
2023-02-07 02:18:44 +0530

    Solution to Problem 1

---
+++ answers/Solution1.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE challenge;  
+USE weather_challenge;
+CREATE TABLE weather_station (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  station_id INTEGER NOT NULL,
+  name TEXT NOT NULL,
+  location TEXT NOT NULL
+);
+
+CREATE TABLE weather_data (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  station_id INTEGER NOT NULL,
+  date TEXT NOT NULL,
+  max_temp REAL NOT NULL,
+  min_temp REAL NOT NULL,
+  precipitation REAL NOT NULL,
+  FOREIGN KEY (station_id) REFERENCES weather_station (id)
+);
